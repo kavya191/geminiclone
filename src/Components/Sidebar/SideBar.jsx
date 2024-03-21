9051760ac19b21750039d06ab12d83de08c9eef2
kavya191 <kavyabose191@gmail.com>
2024-03-21 21:36:06 +0530

    adding functionality

--- src/Components/Sidebar/SideBar.jsx
+++ src/Components/Sidebar/SideBar.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './SideBar.css'
+import { Context } from '../../Context/GeminiContext';
 const SideBar = () => {
-    const [extend, setExtend] = useState(false)
+    const [extend, setExtend] = useState(false);
+    const {onSent,previousPrompt,setRecentPrompt,newChat}=useContext(Context)
+
+    const loadPrompt=async(prompt)=>{
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
     return (
         <div className='sideBar'>
             <div className="top">
                 <img onClick={()=>setExtend(prev=>!prev)} className='menu' src="https://i.postimg.cc/PJP6FbSm/menu-icon.png" alt="" />
-                <div className="new_chat">
+                <div onClick={()=>newChat()} className="new_chat">
                     <img src="https://i.postimg.cc/zf8Jh3fC/plus-icon.png" alt="" />
                     {extend ? <p>New Chat</p> : null}
 
@@ -14,10 +21,15 @@ const SideBar = () => {
                 {
                     extend ? <div className="recent">
                         <p className="recent_title">Recent</p>
-                        <div className="recent_entry">
-                            <img src="https://i.postimg.cc/nz5b3Lnw/message-icon.png" alt="" />
-                            <p>What is react ?</p>
-                        </div>
+                        { previousPrompt.map((item,index)=>{
+                            return(
+                                <div onClick={()=>loadPrompt(item)} className="recent_entry">
+                                <img src="https://i.postimg.cc/nz5b3Lnw/message-icon.png" alt="" />
+                                <p>{item.slice(0,18)} ...</p>
+                            </div>
+                            )
+                        })}
+                       
                     </div> : null
                 }
 
--- src/Context/GeminiContext.js
+++ src/Context/GeminiContext.js
@@ -10,12 +10,47 @@ export const ContextProvider=(props)=>{
     const[loading,setLoading]=useState(false); //loading animation
     const[resultData,setResultData]=useState("") //display result
 
+     const delayPara=(index,nextWord)=>{
+        setTimeout(function(){
+            setResultData(prev=>prev+nextWord)
+        },75*index)
+     }
+     const newChat=()=>{
+        setLoading(false)
+        setShowResult(false)
+     }
     const onSent=async(prompt)=>{
+
         setResultData("")
         setLoading(true)
         setShowResult(true)
-      const response=  await runChat(input)
-        setResultData(response)
+        let response;
+        if(prompt !== undefined){
+            response= await runChat(prompt)
+            setRecentPrompt(prompt)
+        }else{
+            setPreviousPrompt(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response=await runChat(input)
+        }
+        setRecentPrompt(input)
+       
+        let responseArray=response.split("**")
+        let newResponse;
+        for(let i = 0; i<responseArray.length;i++){
+            if(i === 0 || i%2 !== 1){
+                newResponse += responseArray[i]
+            }
+            else{
+                newResponse += "<b>"+responseArray[i]+"</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        let newResponseArray=newResponse2.split(" ");
+        for(let i=0;i<newResponseArray.length;i++){
+            const nextWord=newResponseArray[i];
+            delayPara(i,nextWord+" ")
+        }
         setLoading(false)
         setInput("")
     
@@ -31,7 +66,8 @@ export const ContextProvider=(props)=>{
         showResult,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
 
     }
     return(
