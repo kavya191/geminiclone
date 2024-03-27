cd8fa79240ed6aecb7bdbeb3429883e4531940a1
kavya191 <kavyabose191@gmail.com>
2024-03-27 08:36:14 +0530

    changes committed

--- src/Context/GeminiContext.js
+++ src/Context/GeminiContext.js
@@ -36,7 +36,7 @@ export const ContextProvider=(props)=>{
         setRecentPrompt(input)
        
         let responseArray=response.split("**")
-        let newResponse;
+        let newResponse="";
         for(let i = 0; i<responseArray.length;i++){
             if(i === 0 || i%2 !== 1){
                 newResponse += responseArray[i]
@@ -51,6 +51,7 @@ export const ContextProvider=(props)=>{
             const nextWord=newResponseArray[i];
             delayPara(i,nextWord+" ")
         }
+      
         setLoading(false)
         setInput("")
     
